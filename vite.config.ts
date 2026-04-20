e89ac8d055bd0b04b137105ab27183d7d9850dd5
zenzen26 <thamzien@gmail.com>
2026-04-20 16:03:38 +1000

    (feat) added github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base:'/Goya-Museum/',
   plugins: [
     react(),
     tailwindcss(),
